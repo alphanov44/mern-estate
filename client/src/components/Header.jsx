2ac1c823241c73bf09746639dc54533561a2ab09
Dr Prakash V N <nandapurkaraditi8@gmail.com>
2024-09-23 00:55:00 +0530

    create header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,10 @@
-import {FaSearch} from 'react-icons';
+import {FaSearch} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w6xl mx-auto p-3'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Sahand</span>
@@ -12,27 +12,22 @@ export default function Header() {
           </h1>
         </Link>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-          <input 
-          type="text" 
-          placeholder="Search..." 
-          className='bg-transparent focus:outline-none w-24 sm:w-64' 
-          />
+          <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
           <FaSearch className='text-slate-600' />
         </form>
         <ul className='flex gap-4'>
-          <Link to=' /about'>
-              <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+          <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
           </Link>
-          <Link to='/about '>
-              <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+          <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
           </Link>
           <Link to='/sign-in'>
-              <li className='text-slate-700 hover:underline'>Sign IN</li>
+            <li className='text-slate-700 hover:underline'>Sign in</li>
           </Link>
-          
         </ul>
+
       </div>
-      
     </header>
   )
 }
